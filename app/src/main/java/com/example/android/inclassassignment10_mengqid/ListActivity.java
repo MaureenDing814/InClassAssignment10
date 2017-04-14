b0623dae4e4dc8ae55d153510b2389be2455e259
Maureen_Ding <md3474@tc.columbia.edu>
2017-04-13 20:35:01 -0400

    Change into Recycle View

--- app/src/main/java/com/example/android/inclassassignment10_mengqid/ListActivity.java
+++ app/src/main/java/com/example/android/inclassassignment10_mengqid/ListActivity.java
@@ -2,6 +2,8 @@ package com.example.android.inclassassignment10_mengqid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,6 +19,9 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 import static android.os.Build.VERSION_CODES.N;
 
 public class ListActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     TextView display;
 
@@ -35,20 +40,26 @@ public class ListActivity extends AppCompatActivity {
 
         posts = new ArrayList<>();
 
-        display = (TextView) findViewById(R.id.display);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(posts);
+        mRecyclerView.setAdapter(mAdapter);
 
         postRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 BlogPost post = dataSnapshot.getValue(BlogPost.class);
                 posts.add(post);
-
-                String results = "";
-                for(BlogPost p:posts)
-                {
-                    results += p + "\n";
-                }
-                display.setText(results);
+                mAdapter.notifyDataSetChanged();
             }
             // When it detects that some children are added
 
